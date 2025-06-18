3cdd3c8238801077ab7191727853cf0cae9473f7
Mona Alrayes <=>
2025-06-18 23:36:14 +0300

    'sixth commit'

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
+
+# Install system build dependencies
 sudo apt-get update
 sudo apt-get install -y build-essential
+
+# Ensure pip is upgraded
+python -m pip install --upgrade pip
+
+# Install Python dependencies
 pip install -r requirements.txt
