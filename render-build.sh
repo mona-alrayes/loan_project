1dfdb2f41d93883f98e4aabd2801dfd90ba274c6
Mona Alrayes <=>
2025-06-19 00:15:15 +0300

    'tenth commit'

--- render-build.sh
+++ render-build.sh
@@ -1,15 +1,8 @@
-#!/usr/bin/env bash
-set -o errexit
-
-# Install system build dependencies without sudo
-echo "Installing build dependencies..."
-apt-get update -y
-apt-get install -y build-essential python3-dev
+#!/bin/bash
+set -e  # Exit on error
 
 # Upgrade pip and setuptools
-echo "Upgrading pip and setuptools..."
 python -m pip install --upgrade pip setuptools wheel
 
 # Install requirements
-echo "Installing Python dependencies..."
 pip install -r requirements.txt
