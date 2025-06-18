ac5f2a700326195da1fce4709d81209418cada1b
Mona Alrayes <=>
2025-06-18 19:40:57 +0300

    first commit

---
+++ app/database.py
@@ -0,0 +1,19 @@
+import os  # Add this import at the top
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Get database URL from environment variable
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./loan_app.db")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def create_tables():
+    """Create database tables if they don't exist"""
+    Base.metadata.create_all(bind=engine)
