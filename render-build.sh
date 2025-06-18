ea1c1a125716f8652551c801012c64ba856c75db
Mona Alrayes <=>
2025-06-19 01:51:08 +0300

    '13th commit'

--- render-build.sh
+++ render-build.sh
@@ -7,5 +7,8 @@ python -m pip install --upgrade pip setuptools wheel
 # Install pandas separately first with no binary checks
 pip install pandas==2.3.0 --no-binary=pandas
 
+# Install SQLAlchemy from GitHub main branch
+pip install git+https://github.com/sqlalchemy/sqlalchemy.git@main
+
 # Install remaining dependencies
 pip install -r requirements.txt
