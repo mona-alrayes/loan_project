dd17d6277a4b00c22ea8924c77a18988ca0379d0
Mona Alrayes <=>
2025-06-19 00:18:47 +0300

    'eleventh commit'

--- render-build.sh
+++ render-build.sh
@@ -4,5 +4,8 @@ set -e  # Exit on error
 # Upgrade pip and setuptools
 python -m pip install --upgrade pip setuptools wheel
 
-# Install requirements
+# Install pandas separately first with no binary checks
+pip install pandas==2.3.0 --no-binary=pandas
+
+# Install remaining dependencies
 pip install -r requirements.txt
